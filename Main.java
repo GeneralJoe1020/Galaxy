a0418f38a7e39ebeefc9f5d9c22ecf7154a4bfe9
Galaxy <whahrud123@gmail.com>
2020-04-22 11:42:13 +0900

    init

---
+++ Main.java
@@ -0,0 +1,32 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	//	boolean bl = 10 >5 ;
+	//	System.out.println("bl="+bl);
+		
+	//	bl = false;
+	//	System.out.println("bl="+bl);  
+	//	
+	//	int num = 10;
+	//	int num1 = num;
+	//	System.out.println("num="+num);
+	//	System.out.println("num1="+num);
+		
+	//	int num2=10,num3 = 10;
+		
+	//	System.out.println("num2+num3 ="+(num2+num3));
+//	}
+	byte b = 127;
+	System.out.println("b="+ ++b);
+	
+	short s = 0x7fff;
+	System.out.println("s="+ ++s);
+	
+	int i = 0x7ffffff;
+	System.out.println("i="+ ++i);
+	long z = 0x7fffffffffffffL;
+	System.out.println("z="+ ++z);
+			}
+}
